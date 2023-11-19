ed4383d2688dc6b25145e1a8fba23e07fb237e13
Allexus Technology <ale988240769@gmail.com>
2023-11-19 11:12:52 -0300

    corrigindo o input com centavos x3

---
+++ build/precache-manifest.993a7b1feeef4076b6c3ea5e23052baf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1c38652391b62a8936ee134c0646d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deb93fcdf66ca2cb6cc0",
+    "url": "/static/css/2.f416a57d.chunk.css"
+  },
+  {
+    "revision": "deb93fcdf66ca2cb6cc0",
+    "url": "/static/js/2.30c2c3da.chunk.js"
+  },
+  {
+    "revision": "2c54910a09663cb7383ac13bbcfd98ab",
+    "url": "/static/js/2.30c2c3da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cdf0da906a45d24dce6",
+    "url": "/static/js/main.d8885230.chunk.js"
+  },
+  {
+    "revision": "3d82551b7646743cd6d2",
+    "url": "/static/js/runtime-main.32855bd5.js"
+  },
+  {
+    "revision": "b0dc21936e27b98d6c65bd72dd705c65",
+    "url": "/static/media/bootstrap-icons.b0dc2193.woff"
+  },
+  {
+    "revision": "c5787b4ab55835580294696c3cbcc521",
+    "url": "/static/media/bootstrap-icons.c5787b4a.woff2"
+  },
+  {
+    "revision": "ece0e37c27d721fe4c8c4bb6b3e09e41",
+    "url": "/static/media/google-icon.ece0e37c.svg"
+  },
+  {
+    "revision": "110792eabe1b75fb86bb24c0a01e421d",
+    "url": "/static/media/logo.110792ea.png"
+  },
+  {
+    "revision": "65fc2ba2e5e72449e0c8d883b390f331",
+    "url": "/static/media/logo.65fc2ba2.svg"
+  }
+]);
